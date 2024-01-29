6231a8f4495fcffdefc75315784a207f14988b12
Zerargui <hakim.zerargui@hotmail.com>
2024-01-29 12:52:16 +0100

    Implémentation des 5 attractions les plus proches

--- TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideController.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/TourGuideController.java
@@ -1,8 +1,16 @@
 package com.openclassrooms.tourguide;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.openclassrooms.tourguide.service.RewardsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +22,7 @@ import com.openclassrooms.tourguide.service.TourGuideService;
 import com.openclassrooms.tourguide.user.User;
 import com.openclassrooms.tourguide.user.UserReward;
 
+import rewardCentral.RewardCentral;
 import tripPricer.Provider;
 
 @RestController
@@ -21,6 +30,8 @@ public class TourGuideController {
 
 	@Autowired
 	TourGuideService tourGuideService;
+    RewardsService rewardsService;
+    RewardCentral rewardCentral;
 	
     @RequestMapping("/")
     public String index() {
@@ -41,11 +52,40 @@ public class TourGuideController {
         // The distance in miles between the user's location and each of the attractions.
         // The reward points for visiting each Attraction.
         //    Note: Attraction reward points can be gathered from RewardsCentral
-    @RequestMapping("/getNearbyAttractions") 
-    public List<Attraction> getNearbyAttractions(@RequestParam String userName) {
-    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
-    	return tourGuideService.getNearByAttractions(visitedLocation);
+    @RequestMapping("/getNearbyAttractions")
+    public ResponseEntity<Object> getNearbyAttractions(@RequestParam String userName) {
+        try {
+            VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
+            List<Attraction> nearbyAttractions = tourGuideService.getNearByAttractions(visitedLocation);
+
+
+            // Create a new JSON object with the required information
+            ObjectMapper objectMapper = new ObjectMapper();
+            ObjectNode result = objectMapper.createObjectNode();
+            ArrayNode attractionsArray = result.putArray("attractions");
+
+            for (Attraction attraction : nearbyAttractions) {
+                ObjectNode attractionNode = attractionsArray.addObject();
+                attractionNode.put("attractionName", attraction.attractionName);
+                attractionNode.put("latitude", attraction.latitude);
+                attractionNode.put("longitude", attraction.longitude);
+                attractionNode.put("userLatitude", visitedLocation.location.latitude);
+                attractionNode.put("userLongitude", visitedLocation.location.longitude);
+
+                double distance = rewardsService.getDistance(visitedLocation.location, attraction);
+                attractionNode.put("distance", distance);
+
+                int rewardPoints = rewardCentral.getAttractionRewardPoints(attraction.attractionId, getUser(userName).getUserId());
+                attractionNode.put("rewardPoints", rewardPoints);
+            }
+
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing JSON");
+        }
     }
+
     
     @RequestMapping("/getRewards") 
     public List<UserReward> getRewards(@RequestParam String userName) {
